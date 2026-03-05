830a059dbc3b5304b30d3447dc7e3b6ec8b41baa
kmaza1 <mazakossivirenaud@gmail.com>
2026-03-05 13:12:12 +0100

    Dossier des fichiers python de FreeCodeCamp

---
+++ FreeCodeCamp_Pyton_Learning/Atelier1.py
@@ -0,0 +1,26 @@
+"""
+============================================================
+   Description   : Building a simple report card printer.
+   Auteur        : MAZA Kossivi Renaud
+   Date          : 04/03/2025
+   Niveau        : 2ème année Prépa Informatique
+   Établissement : École Polytechnique de Lomé
+   Environnement :pycharm
+   Langage       : python 3.14
+   Licence       : Freecodecamp
+
+ ============================================================
+"""
+
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
---
+++ FreeCodeCamp_Pyton_Learning/Atelier4.py
@@ -0,0 +1,14 @@
+"""
+============================================================
+   Description   : Building a movie ticket booking calculator.
+   Auteur        : MAZA Kossivi Renaud
+   Date          : 04/03/2025
+   Niveau        : 2ème année Prépa Informatique
+   Établissement : École Polytechnique de Lomé
+   Environnement :pycharm
+   Langage       : python 3.14
+   Licence       : Freecodecamp
+
+ ============================================================
+"""
+
